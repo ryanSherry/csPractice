120999af5c20bf26a9530f2ced5bd078d569f829
Ryan Sherry <rsherrycomputers@gmail.com>
2019-02-21 16:53:07 -0700

    refactored to make code cleaner

--- src/SpiralProblem.java
+++ src/SpiralProblem.java
@@ -2,10 +2,14 @@ public class SpiralProblem {
     private int[][] data;
     private int numRows;
     private int numCol;
+    private int commaLimit;
+
+
     public SpiralProblem(int numRows, int numCol) {
         this.numRows = numRows;
         this.numCol = numCol;
         this.data = new int[numRows][numCol];
+        this.commaLimit = numCol * numRows - 1;
         int counter = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -15,46 +19,39 @@ public class SpiralProblem {
         }
     }
 
+    private void printHelper(int counter, int value) {
+        if (counter < commaLimit) {
+            System.out.print(value + ", ");
+        } else {
+            System.out.print(value);
+        }
+    }
+
         public void printArray(int counter, int start, int finish){
 
+        //base case
         if (counter == numCol * numRows) {
             return;
         }
 
             for(int i = start; i < finish; i++) {
-                if(counter == numCol * numRows - 1) {
-                    System.out.print(data[start][i]);
-                } else {
-                    System.out.print(data[start][i] + ", ");
-                }
+                printHelper(counter, data[start][i]);
                 counter++;
             }
 
 
             for(int i = start+1; i < finish; i++) {
-                if(counter == numCol * numRows - 1) {
-                    System.out.print(data[i][finish - 1]);
-                } else {
-                    System.out.print(data[i][finish - 1] + ", ");
-                }
+                printHelper(counter, data[i][finish - 1]);
                 counter++;
             }
 
             for(int i = finish - 1 - 1; i >= start; i--) {
-                if(counter == numCol * numRows - 1) {
-                    System.out.print(data[finish - 1][i]);
-                } else {
-                    System.out.print(data[finish - 1][i] + ", ");
-                }
+                printHelper(counter, data[finish - 1][i]);
                 counter++;
             }
 
             for(int i = finish - 1 - 1; i > start; i--) {
-                if (counter == numCol * numRows - 1) {
-                    System.out.print(data[i][start]);
-                } else {
-                    System.out.print(data[i][start] + ", ");
-                }
+                printHelper(counter, data[i][start]);
                 counter++;
             }
 
